7c803d17f26659a43d59ac2513cea360d4f79b1a
n0aaz <aazdu03@gmail.com>
2020-12-01 17:05:44 +0100

    Update SRGAN.py

--- SRGAN.py
+++ SRGAN.py
@@ -40,12 +40,41 @@ class SRGAN():
             optimizer=optimizer,
             metrics=['accuracy'])        
 
+        # Configure data loader
+        self.dataset_name = 'img_align_celeba'
+        self.data_loader = DataLoader(dossier=self.dataset_name,
+                                      resolution=(self.hr_height, self.hr_width))
+        
+        # Calculate output shape of D (PatchGAN)
+        patch = int(self.hr_height / 2**4)
+        self.disc_patch = (patch, patch, 1)    
+
         self.discriminateur= Discriminator(hr_shape).build()
         self.discriminateur.compile(loss='mse',
             optimizer=optimizer,
             metrics=['accuracy'])
 
         self.generateur= Generator(lr_shape).build()
+
+        # Images (entrées) haute résolution & basse résolution
+        img_highres = Input(shape=self.hr_shape)
+        img_lowres = Input(shape=self.lr_shape)
+
+        # Haute résolution générée par le générateur:
+        gen_highres = self.generator(img_lowres)
+
+        # Features de l'image du générateur
+        gen_features= self.vgg(gen_highres)
+
+        
+        self.discriminateur.trainable =False
+
+        validite = self.discriminateur(gen_highres)
+        self.combinaison = Model([img_lowres,img_highres],[validite,gen_features])
+        self.combinaison.compile(loss=['binary_crossentropy', 'mse'],
+                              loss_weights=[1e-3, 1],
+                              optimizer=optimizer)
+
 
 
     def build(self):
