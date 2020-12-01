8ee6e18532b72d93b808e5efa9a7c8864b58a2a9
n0aaz <aazdu03@gmail.com>
2020-12-01 14:59:28 +0100

    Liste des importations
    Creation du fichier principal avec toutes les importations nécéssaires.

---
+++ SRGAN.py
@@ -0,0 +1,24 @@
+# Liste des imports dont on pourra éventuellement avoir besoin
+import scipy
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+#from tensorflow.keras_contrib.layers.normalization.instancenormalization import InstanceNormalization
+from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
+from tensorflow.keras.layers import BatchNormalization, Activation, ZeroPadding2D, Add
+from tensorflow.keras.layers import PReLU, LeakyReLU
+from tensorflow.keras.layers import UpSampling2D, Conv2D
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import Adam
+import datetime
+import matplotlib.pyplot as plt
+import sys
+from data_loader1 import DataLoader
+import numpy as np
+import os
+
+import tensorflow.keras.backend as K
+
+class SRGAN():
+    def __init__(self):
+        
