14525a8c853ab764ab279df8f4addaeff0f55601
Amande16340 <66057868+Amande16340@users.noreply.github.com>
2020-12-01 16:04:57 +0100

    Update Discriminator
    modification build et init

--- Discriminator.py
+++ Discriminator.py
@@ -21,26 +21,16 @@ import tensorflow.keras.backend as K
 
 class Discriminator():
     
-    def __init__(self, lr_shape,hr_shape) :
+    def __init__(self) :
         
         #Construction et Compilation du discriminateur
-        self.discriminator = self.build_discriminator()
-        self.discriminator.compile(loss='mse',optimizer=optimizer, metrics=['accuracy'])
-    
-        # High res. and low res. images
-        img_hr = Input(shape=self.hr_shape)
-        img_lr = Input(shape=self.lr_shape)
-        
-    #Discriminateur détermine validité de la génération d'image HR
-        validity = self.discriminator(fake_hr)
-        self.combined = Model([img_lr, img_hr], [validity, fake_features])
-        self.combined.compile(loss=['binary_crossentropy', 'mse'], loss_weights=[1e-3, 1],optimizer=optimizer)
+        self.discriminateur = build()
+        self.discriminateur.compile(loss='mse',optimizer=optimizer, metrics=['accuracy'])
     
     def dicriminateur_bloc(layer_entree, filtres,bn=True) :
         #Ecriture du bloc réseau décrit dans la doc
         d = Conv2D(filtres, kernel=3,strides=1,padding='same')(layer_entree)
         d = LeakyReLU(alpha = 0.2)(d)
-        #Utilisation de la fonction d'activation LeakyReLU + efficace que ReLU "
         
         if bn :
             d = BatchNormalization(momentum=0.8)(d)
@@ -65,7 +55,7 @@ class Discriminator():
         
         return d10
     
-    def build_Discriminateur(self) :
+    def build(self) :
         
         resultat_reseau = discriminateur_reseau(self)
         validity = Dense(1, activation='sigmoid')(resultat_reseau)
