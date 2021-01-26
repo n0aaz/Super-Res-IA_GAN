edf5442915436c94ba790e93facdd53695d302fc
Serdar Ozdemir <sozde@compute-gpu-02.lan>
2021-01-27 00:54:23 +0100

    Création d'un script python rudimentaire pour pouvoir lancer l'entrainement pendant la nuit On ne peut pas laisser Jupyter tourner en fond sans y être connecté en SSH donc autant lancer un script python depuis un terminal et rediriger la sortie grâce à un pipe bash vers un log.
    La commande à utiliser est " conda run python ScriptNocturne.py > logNocturne.txt "
    
    Bonne nuit et on verra demain si notre modele s'entraine bien...

---
+++ Script_Nocturne.py
@@ -0,0 +1,9 @@
+### ATTENDRE QUELQUES HEURES QUE LE SERVEUR AIT FINI DE BOUGER LES IMAGES
+from SRGAN import SRGAN as srgan
+from time import sleep 
+sleep(3600*3)
+
+SR=srgan()
+
+SR.train(10000)
+SR.sauvegarde_modele()
