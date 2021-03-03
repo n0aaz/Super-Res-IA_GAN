483f0fa4a61cc71e1484465f8b1cc1e8e2e3e2d7
n0aaz <aazdu03@gmail.com>
2021-03-03 10:06:46 +0100

    Repasser en 224*224
    Etrangement, avec une image en 384² l'entrainement se fait parfaitement depuis Jupyter mais pas en lancant le script d'entrainement.

--- SRGAN.py
+++ SRGAN.py
@@ -30,8 +30,8 @@ class SRGAN():
         
         self.facteur_reduction=4
 
-        self.hr_height = 384 #self.lr_height*4   # High resolution height
-        self.hr_width = 384 #self.lr_width*4     # High resolution width
+        self.hr_height = 224 #self.lr_height*4   # High resolution height
+        self.hr_width = 224 #self.lr_width*4     # High resolution width
         self.lr_height = self.hr_height//self.facteur_reduction #64                 # Low resolution height
         self.lr_width = self.hr_width//self.facteur_reduction #64                  # Low resolution width
         self.lr_shape = (self.lr_height, self.lr_width, self.channels)
