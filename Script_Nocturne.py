d0b26e1ba0ee349da6f326aa7e446071917f84c3
Serdar Ozdemir <sozde@compute-gpu-03.lan>
2021-03-16 13:49:54 +0100

    Faire varier le learning rate quand on lance un long training

--- Script_Nocturne.py
+++ Script_Nocturne.py
@@ -1,17 +1,25 @@
 ### ATTENDRE QUELQUES HEURES QUE LE SERVEUR AIT FINI DE BOUGER LES IMAGES
 from SRGAN import SRGAN as srgan
+from tensorflow.keras.optimizers import Adam
 #from time import sleep 
 #sleep(3600*3)
 
 SR=srgan()
-SR.train_batch_size = 5 # temps par génération : environ 3s
+SR.train_batch_size = 6
 
-temps_generation = 3 # en secondes
-temps_sauvegarde = 900 # 4min
-temps_entrainement = 10*3600 # 10h
+temps_generation = 1.9 # en secondes
+temps_sauvegarde = 300 # 5min
+temps_entrainement = 48*3600 # 10h
 
 SR.auto_sauvegarde= True
-SR.intervalle_sauvegarde= temps_sauvegarde//temps_generation # temps par sauvegarde : environ 7s, une sauvegarde toutes les 4min
+SR.intervalle_sauvegarde= int(temps_sauvegarde//temps_generation) # temps par sauvegarde : environ 7s, une sauvegarde toutes les 4min
+
+# Changement de learning rate: 
+
+SR.adam_learning_rate/= 100
+SR.optimizer= Adam(learning_rate=SR.adam_learning_rate,beta_1=SR.adam_beta1,beta_2=SR.adam_beta2,epsilon=SR.adam_epsilon)
+
+
 SR.charger_modeles()
-SR.train(temps_entrainement//temps_generation) # 24h d'entrainement, autosauvegarde en +
+SR.train(int(temps_entrainement//temps_generation)) # 24h d'entrainement, autosauvegarde en +
 
